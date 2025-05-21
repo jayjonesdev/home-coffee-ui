6340212296c67100e069c5853bd6d15dcf383dfd
Jerrell Jones <jayjonesdevelopment@gmail.com>
2025-05-21 16:43:10 -0500

    create barista and customer routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from 'react-router';
-import { Home } from './Home';
+import { Home } from './pages/home';
+import { Order } from './pages/customer/order';
+import { Orders } from './pages/barista/orders';
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Home />,
+	},
+	{
+		path: '/order',
+		element: <Order />,
+	},
+	{
+		path: '/orders',
+		element: <Orders />,
 	},
 ]);
