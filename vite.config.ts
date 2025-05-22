cb7c5e86279e072bfc3ea5f61fb7ec3d4275ec07
Jerrell Jones <jayjonesdevelopment@gmail.com>
2025-05-21 21:21:40 -0500

    update vite test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,30 @@
 import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
 import { defineConfig as defineVitestConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 const viteConfig = defineViteConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+});
 
 const vitestConfig = defineVitestConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: "setup.ts",
-     coverage: {
-      thresholds: {
-        lines: 55,
-        branches: 55,
-        functions: 55,
-        statements: 55
-      },
-      // you can include other reporters, but 'json-summary' is required, json is recommended
-      reporter: ['text', 'json-summary', 'json'],
-      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
-      reportOnFailure: true,
-    }
-  }
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: 'setup.ts',
+		coverage: {
+			thresholds: {
+				lines: 85,
+				branches: 85,
+				functions: 85,
+				statements: 85,
+			},
+			// you can include other reporters, but 'json-summary' is required, json is recommended
+			reporter: ['text', 'json-summary', 'json'],
+			// If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+			reportOnFailure: true,
+		},
+	},
 });
 
 export default mergeConfig(viteConfig, vitestConfig);
