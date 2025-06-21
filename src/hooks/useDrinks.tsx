1fc2687bd7505002675c8996e823983dacc30df0
Jerrell Jones <jayjonesdevelopment@gmail.com>
2025-06-20 22:29:16 -0500

    add hook to show different drinks

---
+++ src/hooks/useDrinks.tsx
@@ -0,0 +1,25 @@
+export const useDrinks = () => {
+    return [
+        {
+            name: 'Latte',
+            options: {
+                'Milk': ['Oat', 'Whole'],
+                'Flavor': ['None', 'Vanilla', 'Cardomom', 'Pistachio'],
+                'Temperature': ['Hot', 'Iced']
+            }
+        }, {
+            name: 'Espresso',
+            options: {
+                'Size': ['Single', 'Double'],
+            }
+        },
+        {
+            name: 'Pour Over',
+            options: {
+                'Type': ['Ethopia', 'Kenya']
+            }
+        }
+    ]
+};
+
+export default useDrinks;
--- src/pages/customer/steps/Order.tsx
+++ src/pages/customer/steps/Order.tsx
@@ -1,5 +1,6 @@
 import {
 	Box,
+	Button,
 	FormControl,
 	FormControlLabel,
 	FormLabel,
@@ -9,14 +10,28 @@ import {
 	TextField,
 	Typography,
 } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import useDrinks from '../../../hooks/useDrinks';
+import { Add } from '@mui/icons-material';
 
 export const Order = () => {
+	const drinks = useDrinks();
+	const [showMenu, setShowMenu] = useState(false);
+	const order = [];
+
+	const addDrinkClick = () => setShowMenu(true)
+
 	return (
-		<div style={{ display: 'flex', flexDirection: 'column' }}>
+		<div style={{ display: 'flex', flexDirection: 'column', width: '50%', height: '100%', paddingBlock: 50 }}>
 			<TextField required id='outlined-required' label='Name' />
-			<FormControl>
-				<FormLabel id='demo-radio-buttons-group-label'>Drink</FormLabel>
+			<Button fullWidth startIcon={<Add />} onClick={addDrinkClick}>Add drink</Button>
+			{showMenu && <FormControl>
+				{drinks.map((drink) => {
+					return (
+						<FormLabel id='drink-name'>{drink.name}</FormLabel>
+					)
+				})}
+				{/* <FormLabel id='demo-radio-buttons-group-label'>Drink</FormLabel>
 				<RadioGroup
 					aria-labelledby='demo-radio-buttons-group-label'
 					defaultValue='female'
@@ -35,8 +50,8 @@ export const Order = () => {
 					/>
 					<FormControlLabel value='tea' control={<Radio />} label='Tea' />
 					<FormControlLabel value='matcha' control={<Radio />} label='Matcha' />
-				</RadioGroup>
-			</FormControl>
+				</RadioGroup> */}
+			</FormControl>}
 		</div>
 	);
 };
