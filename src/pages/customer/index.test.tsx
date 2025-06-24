7631a67ade41d408e428c0fa533f9da2437ad204
Jerrell Jones <jayjonesdevelopment@gmail.com>
2025-06-23 21:04:17 -0500

    update test

--- src/pages/customer/index.test.tsx
+++ src/pages/customer/index.test.tsx
@@ -1,11 +1,23 @@
 import { render, screen } from '@testing-library/react';
-import { describe } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { Customer } from '.';
+
+const mockedUseNavigate = vi.fn();
+
+vi.mock('react-router', async () => {
+	const mod = await vi.importActual<typeof import('react-router')>(
+		'react-router',
+	);
+	return {
+		...mod,
+		useNavigate: () => mockedUseNavigate,
+	};
+});
 
 describe('Customer Order', () => {
 	it('should render', () => {
 		render(<Customer />);
 
-		expect(screen.findByText('Order Screen')).toBeTruthy();
+		expect(screen.findByText('Select a drink')).toBeTruthy();
 	});
 });
